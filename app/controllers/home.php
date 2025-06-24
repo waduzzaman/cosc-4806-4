6390221d75d089cd3a79d73503b488fe779815d7
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-06-24 15:06:12 +0000

    Initial commit

---
+++ app/controllers/home.php
@@ -0,0 +1,32 @@
+<?php
+
+class Home extends Controller {
+
+    public function index() {
+        $user = $this->model('User');
+        $data = $user->test();
+
+        // route to index page
+
+        $this->view('home/index');
+        die;
+    }
+    // route to about page
+    public function about() {
+        $this->view('home/about');
+        die;
+    }
+
+    // route to portfolio page
+
+    public function portfolio() {
+        $this->view('home/portfolio');
+        die;
+    }
+    // route to contact page
+
+     public function contact() {
+        $this->view('home/contact');
+        die;
+  }
+}
---
+++ app/views/templates/footer.php
@@ -0,0 +1,33 @@
+<footer class="bg-gray-900 text-white py-6 mt-12">
+  <div class="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+    <p class="text-sm md:text-base text-center md:text-left">
+      &copy; <?php echo date('Y'); ?> Md Mahbub E Waduzzaman All rights reserved.
+    </p>
+
+    <div class="flex space-x-6">
+      <a href="https://facebook.com" target="_blank" class="hover:text-blue-500" aria-label="Facebook">
+        <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24">
+          <path d="M22 12a10 10 0 1 0-11.5 9.87v-6.99H8v-2.88h2.5V9.5c0-2.48 1.48-3.86 3.75-3.86 1.09 0 2.24.2 2.24.2v2.47h-1.26c-1.24 0-1.63.77-1.63 1.56v1.87H17l-.4 2.88h-2.1v6.99A10 10 0 0 0 22 12Z" />
+        </svg>
+      </a>
+
+      <a href="https://twitter.com" target="_blank" class="hover:text-sky-400" aria-label="Twitter">
+        <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24">
+          <path d="M22.46 6c-.77.35-1.6.58-2.46.69a4.3 4.3 0 0 0 1.88-2.37 8.6 8.6 0 0 1-2.72 1.04 4.28 4.28 0 0 0-7.3 3.9A12.15 12.15 0 0 1 3.15 4.9a4.28 4.28 0 0 0 1.33 5.71 4.25 4.25 0 0 1-1.94-.53v.05a4.28 4.28 0 0 0 3.43 4.2 4.3 4.3 0 0 1-1.93.07 4.29 4.29 0 0 0 4 2.98A8.6 8.6 0 0 1 2 19.54 12.13 12.13 0 0 0 8.29 21c7.55 0 11.68-6.26 11.68-11.7 0-.18-.01-.35-.02-.53A8.3 8.3 0 0 0 22.46 6Z" />
+        </svg>
+      </a>
+
+      <a href="https://linkedin.com" target="_blank" class="hover:text-blue-300" aria-label="LinkedIn">
+        <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24">
+          <path d="M4.98 3.5C3.88 3.5 3 4.38 3 5.5s.88 2 1.98 2S7 6.62 7 5.5 6.08 3.5 4.98 3.5Zm-.01 4.75H2v12.25h2.97V8.25Zm4.82 0H6.97v12.25h2.97v-6.58c0-1.57.92-2.38 2.14-2.38s2.02.82 2.02 2.38v6.58H17v-7.09c0-3.27-1.75-4.91-4.26-4.91-1.92 0-2.77 1.06-3.22 1.81V8.25Z" />
+        </svg>
+      </a>
+
+   
+    </div>
+  </div>
+</footer>
+
+</div>
+</body>
+</html>
