b12c9180bf69d7144c7afa99c78fbf5af64ba4f4
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-06-25 00:10:48 +0000

    add delete method to delete the reminder

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -15,19 +15,16 @@ class Reminders extends Controller {
 
       if (!empty($subject)) {
           $db = db_connect();
-          $stmt = $db->prepare("INSERT INTO reminders (subject, user_id) VALUES (:subject, :user_id)");
+          $stmt = $db->prepare("INSERT INTO reminders (subject, user_id) VALUES     (:subject, :user_id)");
           $stmt->bindValue(':subject', $subject);
           $stmt->bindValue(':user_id', $user_id);
           $stmt->execute();
-
           header('Location: /reminders');
           exit;
       }
   }
     $this->view('reminders/create');
   }
-
-
   // udpate reminder
   public function update($id) {
     $R = $this->model('Reminder');
@@ -41,6 +38,19 @@ class Reminders extends Controller {
     $reminder = $R->get_reminder_by_id($id);
     $this->view('reminders/update', ['reminder' => $reminder]);
   }
+
+  // delete reminder
+ 
+  public function delete($id) {
+    $R = $this->model('Reminder');
+    $R->delete_reminder($id);
+    // header('Location: /reminders');
+    // exit;
+    echo '<script>window.location.href="/reminders";</script>';
+    exit;
+  
+  }
+
   
   
 }
